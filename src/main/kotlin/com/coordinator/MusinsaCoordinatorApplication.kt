7eefe4e18e43c71820c5720820df60bb9dd8b3b2
HwanChang <hwanchang.dev@gmail.com>
2025-03-13 22:21:57 +0000

    🎉 프로젝트 생성

---
+++ src/main/kotlin/com/coordinator/MusinsaCoordinatorApplication.kt
@@ -0,0 +1,11 @@
+package com.coordinator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusinsaCoordinatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusinsaCoordinatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/coordinator/MusinsaCoordinatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coordinator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusinsaCoordinatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
