abf1dc4d9d4f0e8cdcfd30446fe32bbdd8a04869
HwanChang <hwanchang.dev@gmail.com>
2025-03-14 23:11:33 +0000

    🌱 seed data 수정

---
+++ src/main/resources/db/migration/V2__insert_seed_data.sql
@@ -0,0 +1,90 @@
+INSERT INTO "brand" ("id", "name") VALUES (1, 'A');
+INSERT INTO "brand" ("id", "name") VALUES (2, 'B');
+INSERT INTO "brand" ("id", "name") VALUES (3, 'C');
+INSERT INTO "brand" ("id", "name") VALUES (4, 'D');
+INSERT INTO "brand" ("id", "name") VALUES (5, 'E');
+INSERT INTO "brand" ("id", "name") VALUES (6, 'F');
+INSERT INTO "brand" ("id", "name") VALUES (7, 'G');
+INSERT INTO "brand" ("id", "name") VALUES (8, 'H');
+INSERT INTO "brand" ("id", "name") VALUES (9, 'I');
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (1, 1, 'A-상품-1', 'TOP', 11200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (2, 1, 'A-상품-2', 'OUTER', 5500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (3, 1, 'A-상품-3', 'TROUSERS', 4200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (4, 1, 'A-상품-4', 'SNEAKERS', 9000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (5, 1, 'A-상품-5', 'BAG', 2000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (6, 1, 'A-상품-6', 'HAT', 1700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (7, 1, 'A-상품-7', 'SOCKS', 1800);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (8, 1, 'A-상품-8', 'ACCESSORY', 2300);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (9, 2, 'B-상품-1', 'TOP', 10500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (10, 2, 'B-상품-2', 'OUTER', 5900);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (11, 2, 'B-상품-3', 'TROUSERS', 3800);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (12, 2, 'B-상품-4', 'SNEAKERS', 9100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (13, 2, 'B-상품-5', 'BAG', 2100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (14, 2, 'B-상품-6', 'HAT', 2000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (15, 2, 'B-상품-7', 'SOCKS', 2000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (16, 2, 'B-상품-8', 'ACCESSORY', 2200);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (17, 3, 'C-상품-1', 'TOP', 10000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (18, 3, 'C-상품-2', 'OUTER', 6200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (19, 3, 'C-상품-3', 'TROUSERS', 3300);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (20, 3, 'C-상품-4', 'SNEAKERS', 9200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (21, 3, 'C-상품-5', 'BAG', 2200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (22, 3, 'C-상품-6', 'HAT', 1900);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (23, 3, 'C-상품-7', 'SOCKS', 2200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (24, 3, 'C-상품-8', 'ACCESSORY', 2100);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (25, 4, 'D-상품-1', 'TOP', 10100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (26, 4, 'D-상품-2', 'OUTER', 5100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (27, 4, 'D-상품-3', 'TROUSERS', 3000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (28, 4, 'D-상품-4', 'SNEAKERS', 9500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (29, 4, 'D-상품-5', 'BAG', 2500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (30, 4, 'D-상품-6', 'HAT', 1500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (31, 4, 'D-상품-7', 'SOCKS', 2400);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (32, 4, 'D-상품-8', 'ACCESSORY', 2000);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (33, 5, 'E-상품-1', 'TOP', 10700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (34, 5, 'E-상품-2', 'OUTER', 5000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (35, 5, 'E-상품-3', 'TROUSERS', 3800);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (36, 5, 'E-상품-4', 'SNEAKERS', 9900);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (37, 5, 'E-상품-5', 'BAG', 2300);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (38, 5, 'E-상품-6', 'HAT', 1800);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (39, 5, 'E-상품-7', 'SOCKS', 2100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (40, 5, 'E-상품-8', 'ACCESSORY', 2100);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (41, 6, 'F-상품-1', 'TOP', 11200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (42, 6, 'F-상품-2', 'OUTER', 7200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (43, 6, 'F-상품-3', 'TROUSERS', 4000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (44, 6, 'F-상품-4', 'SNEAKERS', 9300);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (45, 6, 'F-상품-5', 'BAG', 2100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (46, 6, 'F-상품-6', 'HAT', 1600);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (47, 6, 'F-상품-7', 'SOCKS', 2300);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (48, 6, 'F-상품-8', 'ACCESSORY', 1900);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (49, 7, 'G-상품-1', 'TOP', 10500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (50, 7, 'G-상품-2', 'OUTER', 5800);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (51, 7, 'G-상품-3', 'TROUSERS', 3900);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (52, 7, 'G-상품-4', 'SNEAKERS', 9000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (53, 7, 'G-상품-5', 'BAG', 2200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (54, 7, 'G-상품-6', 'HAT', 1700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (55, 7, 'G-상품-7', 'SOCKS', 2100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (56, 7, 'G-상품-8', 'ACCESSORY', 2000);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (57, 8, 'H-상품-1', 'TOP', 10800);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (58, 8, 'H-상품-2', 'OUTER', 6300);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (59, 8, 'H-상품-3', 'TROUSERS', 3100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (60, 8, 'H-상품-4', 'SNEAKERS', 9700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (61, 8, 'H-상품-5', 'BAG', 2100);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (62, 8, 'H-상품-6', 'HAT', 1600);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (63, 8, 'H-상품-7', 'SOCKS', 2000);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (64, 8, 'H-상품-8', 'ACCESSORY', 2000);
+
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (65, 9, 'I-상품-1', 'TOP', 11400);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (66, 9, 'I-상품-2', 'OUTER', 6700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (67, 9, 'I-상품-3', 'TROUSERS', 3200);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (68, 9, 'I-상품-4', 'SNEAKERS', 9500);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (69, 9, 'I-상품-5', 'BAG', 2400);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (70, 9, 'I-상품-6', 'HAT', 1700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (71, 9, 'I-상품-7', 'SOCKS', 1700);
+INSERT INTO "product" ("id", "brand_id", "name", "category", "price") VALUES (72, 9, 'I-상품-8', 'ACCESSORY', 2400);
